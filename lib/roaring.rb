9c0d4d61e30c22ae21956aeadd7bfe92b50e7870
Chris Yammine <chris@yammine.dev>
2023-02-27 13:43:14 -0800

    rake standard:fix

--- lib/roaring.rb
+++ lib/roaring.rb
@@ -2,16 +2,15 @@
 
 require_relative "roaring/version"
 
-module Roaring
-  class Error < StandardError; end
-  # Your code goes here...
-end
-
-
 # Tries to require the extension for the given Ruby version first
 begin
   RUBY_VERSION =~ /(\d+\.\d+)/
   require "roaring/#{Regexp.last_match(1)}/roaring"
 rescue LoadError
   require "roaring/roaring"
+end
+
+module Roaring
+  class Error < StandardError; end
+  # Your code goes here...
 end
