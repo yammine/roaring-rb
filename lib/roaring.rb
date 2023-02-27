bd044f0aee71890449d84fa0c2dde7e94b796b01
Chris Yammine <chris@yammine.dev>
2023-02-27 13:28:45 -0800

    Try to require te extension for given ruby version first

--- lib/roaring.rb
+++ lib/roaring.rb
@@ -1,9 +1,17 @@
 # frozen_string_literal: true
 
 require_relative "roaring/version"
-require_relative "roaring/roaring"
 
 module Roaring
   class Error < StandardError; end
   # Your code goes here...
+end
+
+
+# Tries to require the extension for the given Ruby version first
+begin
+  RUBY_VERSION =~ /(\d+\.\d+)/
+  require "roaring/#{Regexp.last_match(1)}/roaring"
+rescue LoadError
+  require "roaring/roaring"
 end
